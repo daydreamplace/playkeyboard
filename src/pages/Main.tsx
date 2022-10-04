a950475303b5ca266c5a276b2901f289ab760a3e
kkukileon305 <kkukileon305@gmail.com>
2022-10-04 17:31:28 +0900

    init

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,11 +1,22 @@
+import styled from 'styled-components';
 import Nav from '../components/Nav';
 
 const Main = () => {
   return (
     <>
+      <StyledTitle>
+        <span>Play</span>keyboard
+      </StyledTitle>
       <Nav />
     </>
   );
 };
+
+const StyledTitle = styled.h1`
+  span {
+    color: red;
+    font-weight: bold;
+  }
+`;
 
 export default Main;
