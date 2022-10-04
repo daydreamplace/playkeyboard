18ae86c46e3dab32b4678b1028106664cec72cdd
kkukileon305 <kkukileon305@gmail.com>
2022-10-04 17:20:22 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
+import GlobalStyle from './GlobalStyle';
+import Main from './pages/Main';
+import Theme from './pages/Theme';
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/theme/:id' element={<Theme />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
